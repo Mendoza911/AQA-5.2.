bc774999c65a0635c1ab7c205e78da6813ae60a4
Kirill Markov <isikir668@gmail.com>
2023-07-02 10:30:15 +0300

    Fix build error.

--- src/test/java/org/example/test/AuthTest.java
+++ src/test/java/org/example/test/AuthTest.java
@@ -1,6 +1,7 @@
 package org.example.test;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
